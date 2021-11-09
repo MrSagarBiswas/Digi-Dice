883cca33edd544207e84e1cd71c8219e9c44a427
MrSagarBiswas <biswassagar927@gmail.com>
2021-11-09 19:40:46 +0530

    minor changes

--- javascript.js
+++ javascript.js
@@ -1,24 +1,24 @@
-var point = Math.floor(Math.random()*6+1);
+var point = Math.floor(Math.random() * 6) + 1;
 
 if (point == 6) {
-    for(var i=0; i<6; i++){
-        var j = i+1;
-        document.querySelectorAll(".point")[i].classList.add("point-" +j+ "for6");
+    for (var i = 0; i < 6; i++) {
+        var j = i + 1;
+        document.querySelectorAll(".point")[i].classList.add("point-" + j + "for6");
     }
 } else if (point == 4 || point == 5) {
-    for(var i=0; i<point; i++){
-        var j = i+1;
-        document.querySelectorAll(".point")[i].classList.add("point-" +j);
+    for (var i = 0; i < point; i++) {
+        var j = i + 1;
+        document.querySelectorAll(".point")[i].classList.add("point-" + j);
     }
 } else if (point == 3) {
-        document.querySelectorAll(".point")[0].classList.add("point-2");
-        document.querySelectorAll(".point")[1].classList.add("point-3");
-        document.querySelectorAll(".point")[2].classList.add("point-5");
+    document.querySelectorAll(".point")[0].classList.add("point-2");
+    document.querySelectorAll(".point")[1].classList.add("point-3");
+    document.querySelectorAll(".point")[2].classList.add("point-5");
 } else if (point == 2) {
-        document.querySelectorAll(".point")[0].classList.add("point-2");
-        document.querySelectorAll(".point")[1].classList.add("point-3");
+    document.querySelectorAll(".point")[0].classList.add("point-2");
+    document.querySelectorAll(".point")[1].classList.add("point-3");
 } else if (point == 1) {
-        document.querySelectorAll(".point")[0].classList.add("point-5");
+    document.querySelectorAll(".point")[0].classList.add("point-5");
 }
 
 var youget = "You Get: " + point;
